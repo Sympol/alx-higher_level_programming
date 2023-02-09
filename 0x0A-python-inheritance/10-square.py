b8572a7d1850c67c700d93926d11eb0c7a5bc996
Symplice BONI <bonisimplice@gmail.com>
2023-02-09 04:43:45 +0000

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits Rectangle"""
+
+    def __init__(self, size):
+        """Initialises the attributes: size"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """redefine a area method in the parent class"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """__str__ method for return the next string"""
+
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
