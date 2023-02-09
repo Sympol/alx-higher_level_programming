414c4a778f2b1d02e8afe2ba095b3088bbec6515
Symplice BONI <bonisimplice@gmail.com>
2023-02-09 04:29:20 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class BaseGeometry():
+    """An empty BaseGeometry class"""
+
+    def area(self):
+        """Defines the area of the BaeGeometry class"""
+        raise Exception("area() is not implemented")
