c466db388bc98486d56f6c5f2015b8de2e330771
Symplice BONI <bonisimplice@gmail.com>
2023-02-09 04:35:20 +0000

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """
+    A BaseGeometry class with area
+    """
+
+    def area(self):
+        """
+        definition to calculate area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate the value passed as integer
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0 and type(value) is int:
+            raise ValueError("{} must be greater than 0".format(name))
