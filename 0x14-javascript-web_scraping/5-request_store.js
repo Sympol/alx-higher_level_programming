271ea654479ee8e0bc8bc128446f36ca8daaf9b3
Symplice Boni <bonisimplice@gmail.com>
2023-07-06 13:30:09 +0000

    Del  0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const request = require('request');
-const url = process.argv[2];
-const fileName = process.argv[3];
-request(url).pipe(fs.createWriteStream(fileName));
