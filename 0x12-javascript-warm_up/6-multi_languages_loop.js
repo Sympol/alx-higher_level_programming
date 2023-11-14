1c35876d480d103dcf176dd2142c555400009d91
Symplice BONI <bonisimplice@gmail.com>
2023-11-14 01:02:14 +0000

    js work

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,7 @@
 // Uses an array to loop through and print strings
 
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
 for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
