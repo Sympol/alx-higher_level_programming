141ec5486481d46e819a5c7ff4bf3f2a2a157330
Symplice BONI <bonisimplice@gmail.com>
2023-11-14 00:05:02 +0000

    a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// print multi lines
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+
+console.log(firstLine + '\n' + secondLine + '\n' +
+                thirdLine);
